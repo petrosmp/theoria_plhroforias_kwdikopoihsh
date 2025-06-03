57fef268ba9d0e9c38874b02cbe3dd527a0a4bfc
petrosmp <petrakis798@gmail.com>
2025-06-03 18:00:44 +0300

    Corrected BER calculation to be done with message errors (and not codewords), removed distinct code for R=1

--- homework3/ex_1.m
+++ homework3/ex_1.m
@@ -8,20 +8,23 @@ for rate_idx = 1:length(rates)
     unit = floor(1 / rate);
 
     for n = unit:unit:10*unit
-        num_codewords = 2^floor(n*rate);
+        k = floor(n*rate);
         disp(['rate ', num2str(rate), ', n ', num2str(n), ', unit ', num2str(unit), ...
-              ', (', num2str(num_codewords), ') codewords']);
+              ', (', num2str(2^k), ') codewords']);
 
         n_r_bit_errors = 0;
         parfor i = 1:num_of_experiments            % can be replaced with parfor for faster execution
             random_code = random_N_R_code(n, rate);
             experiment_bit_errors = 0;
             for j = 1:10
-                cw_idx = randi(size(random_code,1));
-                cw = random_code(cw_idx, :);
-                channeled = channel(cw, p);
-                decoded = ML_decode(channeled, random_code);
-                bit_errors = sum(cw ~= decoded);
+
+                message = randi([0 1], k, 1);                           % generate random k-bit message
+                cw_idx = bit2int(message, k)+1;                         % make it decimal so it can be used as an index
+                cw = random_code(cw_idx, :);                            % get the codeword of that index (i.e. of that message)
+                channeled = channel(cw, p);                             % transmit it
+                msg_estimate = ML_decode(channeled, random_code, k);    % estimate the message based on the noisy codeword with ML 
+
+                bit_errors = sum(message ~= msg_estimate);
                 experiment_bit_errors = experiment_bit_errors + bit_errors;
             end
             n_r_bit_errors = n_r_bit_errors + experiment_bit_errors;
@@ -51,13 +54,8 @@ function codewords = random_N_R_code_distinct(n, r)
 end
 
 function codewords = random_N_R_code(n, r)
-    num_codewords = 2^floor(n*r);
-    if r == 1
-        codewords = dec2bin(0:2^n-1, n) - '0';
-        codewords = codewords(randperm(2^n), :);
-    else
+        num_codewords = 2^floor(n*r);
         codewords = randi([0,1], num_codewords, n);
-    end
 end
 
 function output = channel(x, p)
@@ -65,10 +63,10 @@ function output = channel(x, p)
     output = xor(x, flip);
 end
 
-function estimate = ML_decode(received, inputs)
+function estimate = ML_decode(received, inputs, k)
     distances = sum(received ~= inputs, 2);
     [~, min_idx] = min(distances);
-    estimate = inputs(min_idx, :);
+    estimate = int2bit(min_idx+1, k); % make the estimated codeword index back
 end
 
 
